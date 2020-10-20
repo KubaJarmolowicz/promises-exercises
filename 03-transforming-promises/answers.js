20e2ebfe469fc577491af6bb53b31ed7adaebcfb
KubaJarmolowicz <kuba.jarmolowicz92@gmail.com>
2020-10-20 11:40:22 +0100

    Update answers.js

--- 03-transforming-promises/answers.js
+++ 03-transforming-promises/answers.js
@@ -1,44 +1,54 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {*} promise 
- * @param {*} transformer 
+ *
+ * @param {*} promise
+ * @param {*} transformer
  * @returns {Promise}
  */
 function mapPromise(promise, transformer){
   return new Promise((resolve, reject) => {
-    /* IMPLEMENT ME!! */
+    promise.catch(err => reject(err));
+    promise.then(val => transformer(val))
+            .then(newVal => resolve(newVal))
+            .catch(err2 => reject(err2));
+
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
 function squarePromise(numberPromise){
   return numberPromise
-    .then(/* IMPLEMENT ME! */);
+    .then(val =>{
+      if((typeof val=== Number)||(parseFloat(val))) reutrn (parseFloat(val)*parseFloat(val));
+      else throw val;
+    }).catch(err => `Cannot blablabliabla ${err}`);
+
 }
 
 /**
  * EXERCISE 3
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
 function squarePromiseOrZero(promise){
   return squarePromise(promise)
-    .catch(/* IMPLEMENT ME! */);
+    .catch(err=> new Promise(resolve =>{
+      if(typeof err!== Number) resolve(0)
+    }));
 }
 
 /**
  * EXERCISE 4
- * 
- * @param {Promise} promise 
+ *
+ * @param {Promise} promise
  * @returns {Promise}
  */
 function switcheroo(promise){
